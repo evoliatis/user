2e5cdaa56b23930af136d294c519177e8b124a4b
Samuel COLLADO <scollado@evoliatis.com>
2025-09-02 14:29:48 +0200

    Ajout de 2 champs

--- user.go
+++ user.go
@@ -5,16 +5,32 @@ package user
 type User struct {
 	Name  string
 	Login string
+	Email string
+	age   int
 }
 
 // New : create a new user
-func New(name, login string) *User {
-	return &User{Name: name, Login: login}
+func New(name, login, email string) *User {
+	return &User{
+		Name:  name,
+		Login: login,
+		Email: email,
+	}
 }
 
 // UpdateName : update the user's name
 func (u *User) UpdateName(name string) {
 	u.Name = name
+}
+
+// UpdateAge : update age of the user
+func (u *User) UpdateAge(age int) {
+	u.age = age
+}
+
+// GetAge : get age of the user
+func (u User) GetAge() int {
+	return u.age
 }
 
 // GetName : get the user's name
